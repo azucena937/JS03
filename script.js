9488f4549b21b2e7e3bc4485137d4fe1bde363e8
azucena937 <113150657+azucena937@users.noreply.github.com>
2022-10-03 10:07:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -22,4 +22,3 @@ function calcularFecha() {
     const resultElement = document.getElementById('result');
     resultElement.value = `${dayLabel}, ${message}`
 }
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
